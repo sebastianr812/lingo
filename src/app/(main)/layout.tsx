6b096b0777e1aa7b6ec7c772b2efac88e14c4ffd
Sebastian Rojas <sebastian.rojas.dev@gmail.com>
2024-05-23 15:59:33 -0400

    new

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import { MobileHeader } from "@/components/MobileHeader"
-import { Sidebar } from "@/components/Sidebar"
+import { Sidebar } from "../../components/Sidebar"
 
 type Props = {
     children: React.ReactNode
